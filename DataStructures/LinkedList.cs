2bf3406002690c338e78ae4eadab27573134cb2c
vaibhav17ovhal <125484163+vaibhav17ovhal@users.noreply.github.com>
2023-05-14 23:49:34 +0530

    Update LinkedList.cs

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -9,26 +9,7 @@ namespace DataStructures
     public class LinkedList
     {
         public Node head;
-
-        public void Add(int data)
-        {
-            Node node = new Node(data);
-            
-            if (this.head == null)
-            {
-                this.head = node;
-            }
-            else
-            {
-                Node temp = head;
-                while(temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
-            }
-            Console.WriteLine($"{node.data} is inserted into Linkled List");
-        }
+        
         public void Display()
         {
             Node temp = this.head;
