cb1affcd6ca81bfd08e32abf1827aa3e4511669b
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-14 17:12:28 +0530

    [Vaibhav] Add:Creating linked list

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} is inserted into Linkled List");
+        }
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Data Structures");
+
+            LinkedList list = new LinkedList();
+
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+        }
+    }
+}
