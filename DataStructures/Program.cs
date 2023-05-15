85a0b0d89787eafc51fd36e4b134c4859cf6ae46
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-15 20:45:28 +0530

    [Vaibhav] Add:Creating a queue (Enqueue)

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -9,15 +9,11 @@ namespace DataStructures
         {
             Console.WriteLine("Welcome to Data Structures");
 
-            Stack stack = new Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
-
-            stack.Peak();
-            stack.Pop();
-            stack.Display();
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display(); ;
         }
     }
 }
---
+++ DataStructures/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Queue
+    {
+        Node head = null;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
