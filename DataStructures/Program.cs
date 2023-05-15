ccbc873df898086544012169ab871bf5440ac9a4
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-15 20:59:16 +0530

    [Vaibhav] Add:Deleting a queue (Dequeue)

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -13,7 +13,12 @@ namespace DataStructures
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
-            queue.Display(); ;
+            queue.Display();
+
+            Console.WriteLine("\n");
+
+            queue.Dequeue();
+            queue.Display();
         }
     }
 }
--- DataStructures/Queue.cs
+++ DataStructures/Queue.cs
@@ -41,6 +41,21 @@ namespace DataStructures
                 temp = temp.next;
             }
         }
-
+        public void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty, Deletion is not possible");
+                return;
+            }
+            else
+            {
+                while (this.head != null)
+                {
+                    Console.WriteLine("Value Dequeued is {0}", this.head.data);
+                    this.head = this.head.next;
+                }
+            }
+        }
     }
 }
