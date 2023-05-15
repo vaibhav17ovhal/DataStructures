c67031d46f8ea6a8fb8b1d36f679568dcb77c626
Vaibhav <vaibhavovhal15@gmail.com>
2023-05-15 20:03:56 +0530

    [Vaibhav] Add:Creating Stack by pushing

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -9,23 +9,11 @@ namespace DataStructures
         {
             Console.WriteLine("Welcome to Data Structures");
 
-            LinkedList list = new LinkedList();
-
-            list.Add(56);
-            list.Add(30);
-            list.Add(70);
-
-            list.Display();
-
-
-
-            int position = list.Search(30);
-            list.InsertAtParticularPosition(position + 1, 40);
-            list.Display();
-            Console.WriteLine();
-            int searchDelete = list.Search(40);
-            list.DeleteNodeAtParticularPosition(searchDelete);
-            list.Display();
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
         }
     }
 }
---
+++ DataStructures/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class Stack
+    {
+        public Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+    }
+}
